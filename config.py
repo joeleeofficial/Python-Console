46bd80318c40ae9833e09fac9b70a61cbcbe6701
Joe Lee Official <70639311+joeleeofficial@users.noreply.github.com>
2021-01-18 17:11:49 +0800

    Create config.py

---
+++ config.py
@@ -0,0 +1,17 @@
+import os
+
+def echo(msg='',end='\n'):
+    print(msg,end=end)
+
+def python():
+    os.system("python")
+
+def bash():
+    os.system("bash")
+
+def configHelp():
+    print(
+"""
+Hello There, I am Joe. A Programmer, Developer.
+"""
+)
